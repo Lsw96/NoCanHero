31e70b8be43d0e253966cf932ea1fc6bb39f3298
Leeseongwoo <tjddn1007@naver.com>
2024-03-18 22:31:07 +0900

    header, main, sercion(planning) - markup)

---
+++ assets/js/common.js
@@ -0,0 +1,13 @@
+// main-slide
+const slideImg = document.querySelector(".slide-img");
+const clone = slideImg.cloneNode(true);
+
+// add clone
+document.querySelector(".slide-wrap").appendChild(clone);
+
+// original, clone position setting
+document.querySelector(".slide-img").offsetWidth + "px";
+
+// assignment class
+slideImg.classList.add("original");
+clone.classList.add("clone");
